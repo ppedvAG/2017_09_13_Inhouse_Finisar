eba31081a83c34470144e5974170b84b1265677c
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-09-13 10:28:05 +0200

    Factory Method

---
+++ FactoryMethodExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace FactoryMethodExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbProviderFactory factory = SqlClientFactory.Instance;
+
+            using (var connection = factory.CreateConnection())
+            {
+                var connectionString = GetConnectionString();
+            }
+        }
+
+        private static string GetConnectionString()
+        {
+            var server = Console.ReadLine();
+
+            return string.Format("server={0}", server);
+
+            return $"server={server};...";
+        }
+    }
+}
