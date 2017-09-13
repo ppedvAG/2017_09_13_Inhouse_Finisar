d8e5cd558b34aca9f250a9d2ca237218c5875b93
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-09-13 10:10:40 +0200

    Singelton

---
+++ SingeltonExample/Configuration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SingeltonExample
+{
+    internal class Configuration : IDisposable
+    {
+        private static Configuration _instance;
+        public static Configuration Instance => _instance ?? (_instance = new Configuration());
+
+        private Configuration() { }
+
+        public void Load()
+        {
+
+        }
+
+        public void Dispose() => throw new NotImplementedException();
+        }
+    }
+}
--- SingeltonExample/Program.cs
+++ SingeltonExample/Program.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SingeltonExample
+﻿namespace SingeltonExample
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var c = Configuration.Instance;
+            c.Load();
         }
     }
 }
