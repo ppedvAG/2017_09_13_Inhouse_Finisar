62daa1dbb219201bb13ae4718ff308d03c4d04a6
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-09-14 10:53:05 +0200

    Delegates Lambda Expressions Extention Methods

---
+++ DelegatesInPraxis/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DelegatesInPraxis
+{
+    internal delegate bool MyDelegate(Employee employee);
+    // Action       -> void
+    // Predicate<T> -> bool
+    // Func
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = GetData();
+
+            //MyDelegate del = new MyDelegate(Bedinung);
+            //Func<Employee, bool> del = new Func<Employee, bool>(Bedinung);
+            //var del = new Func<Employee, bool>(Bedinung);
+            //Func<Employee, bool> del = Bedinung;
+            //var query = Abfrage(employees, del);
+
+            //var query = Abfrage(employees, Bedingung);
+
+            //var query = Abfrage(employees, delegate (Employee e)
+            //{
+            //    return e.Name.StartsWith("A");
+            //});
+
+            //var query = Abfrage(employees, (Employee e) =>
+            //{
+            //    return e.Name.StartsWith("A");
+            //});
+
+            //var query = Abfrage(employees, (e) =>
+            //{
+            //    return e.Name.StartsWith("A");
+            //});
+
+            //var query = Abfrage(employees, (e) => e.Experience > 8);
+            var query = MyExtentions.MyWhere(employees, e => e.Experience > 8);
+            var query2 = employees.MyWhere(e => e.Experience > 8);
+            var queryLinq = employees.Where(e => e.Experience > 8);
+
+            foreach (var e in query)
+                Console.WriteLine($"Id: {e.Id,2} | {e.Name,-10} | {e.Experience,2}");
+
+            Console.ReadKey();
+        }
+
+        private static bool Bedingung(Employee e)
+        {
+            return e.Name.StartsWith("A");
+        }
+
+        private static IEnumerable<Employee> GetData()
+        {
+            return new HashSet<Employee>
+            {
+                new Employee { Id = 1, Name = "Hannes", Experience = 6 },
+                new Employee { Id = 2, Name = "Lisa", Experience = 7 },
+                new Employee { Id = 3, Name = "Maria", Experience = 10 },
+                new Employee { Id = 4, Name = "Thomas", Experience = 2 },
+                new Employee { Id = 5, Name = "Franz", Experience = 3 },
+                new Employee { Id = 6, Name = "Luise", Experience = 8 },
+                new Employee { Id = 7, Name = "Andrea", Experience = 1 },
+                new Employee { Id = 8, Name = "Max", Experience = 11 },
+                new Employee { Id = 9, Name = "Thomas", Experience = 6 }
+            };
+        }
+    }
+
+    public static class MyExtentions
+    {
+        internal static IEnumerable<T> MyWhere<T>(
+            this IEnumerable<T> employees,
+            Func<T, bool> predicate)
+        {
+            foreach (var e in employees)
+                if (predicate.Invoke(e))
+                    yield return e;
+        }
+    }
+}
---
+++ HalloDelegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HalloDelegates
+{
+    public delegate string MyDelegate(int zahl, double wert);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate del = new MyDelegate(new Program().MeineMethode);
+
+            var result = del.Invoke(4, 9.8);
+
+            Console.WriteLine(result);
+        }
+
+        private string MeineMethode(int i, double d)
+        {
+            return (i + d).ToString();
+        }
+    }
+}
